beca06c5daed5cf0c3db1e46085b45d7c59aaea1
Tino Reichardt <milky-7zip@mcmilk.de>
2016-09-29 21:17:01 +0200

    Ui, forgot to update version to 1.1.0 - next build will have a fix ;)

--- C/7zVersionTr.h
+++ C/7zVersionTr.h
@@ -1,8 +1,8 @@
 #define MY_VER_MAJOR 1
-#define MY_VER_MINOR 0
-#define MY_VER_BUILD 1
-#define MY_VERSION_NUMBERS "1.0.1"
-#define MY_VERSION "1.0.1"
+#define MY_VER_MINOR 1
+#define MY_VER_BUILD 0
+#define MY_VERSION_NUMBERS "1.1.0"
+#define MY_VERSION "1.1.0"
 #define MY_DATE "2016-09-15"
 #undef MY_COPYRIGHT
 #undef MY_VERSION_COPYRIGHT_DATE
