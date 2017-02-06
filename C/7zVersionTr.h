d9764a28d905e35c2d1e60667ad83f2a916bbf8f
Tino Reichardt <milky-7zip@mcmilk.de>
2017-02-06 20:36:51 +0100

    update even more version numbers ;)

--- C/7zVersionTr.h
+++ C/7zVersionTr.h
@@ -1,9 +1,9 @@
 #define MY_VER_MAJOR 1
 #define MY_VER_MINOR 1
-#define MY_VER_BUILD 2
-#define MY_VERSION_NUMBERS "1.1.2"
-#define MY_VERSION         "1.1.2"
-#define MY_DATE            "2016-12-26"
+#define MY_VER_BUILD 3
+#define MY_VERSION_NUMBERS "1.1.3"
+#define MY_VERSION         "1.1.3"
+#define MY_DATE            "2017-02-06"
 #undef MY_COPYRIGHT
 #undef MY_VERSION_COPYRIGHT_DATE
 #define MY_AUTHOR_NAME     "Tino Reichardt"
