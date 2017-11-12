fde3a4629f1a4a7380935bbadd9a27b544c769b9
Tino Reichardt <milky-7zip@mcmilk.de>
2017-11-12 17:44:06 +0100

    Improve logging on AppVeyor

--- CPP/appveyor.cmd
+++ CPP/appveyor.cmd
@@ -1,3 +1,4 @@
+@echo off
 
 REM Microsoft Windows SDK 7.1    (VC=sdk71)
 REM Microsoft Visual Studio 2012 (VC=11.0)
@@ -6,6 +7,7 @@ REM Microsoft Visual Studio 2015 (VC=14.0)
 
 REM to many vcvarsall.cmd calls will blow it up!
 set OPATH=%PATH%
+set ERRFILE=%APPVEYOR_BUILD_FOLDER%\error.txt
 cd %APPVEYOR_BUILD_FOLDER%\CPP
 
 REM I am using sdk71 and 14.0 for releases... /TR
@@ -31,16 +33,17 @@ set VC=14.0
 set NEXT=end
 goto build
 
-
 :build
 FOR /R .\ %%d IN (AMD64 O) DO rd /S /Q %%d 2>NUL
 set PATH=%OPATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio %VC%\VC\vcvarsall.bat" x86
 set OUTDIR=%APPVEYOR_BUILD_FOLDER%\bin-%VC%-x32
-call build-x32.cmd 2>%APPVEYOR_BUILD_FOLDER%\error-%VC%.txt 1>%APPVEYOR_BUILD_FOLDER%\output-%VC%.txt
+echo Building x32
+call build-x32.cmd 1>%APPVEYOR_BUILD_FOLDER%\build-%VC%-x32.txt 2>&1
 call "C:\Program Files (x86)\Microsoft Visual Studio %VC%\VC\vcvarsall.bat" x86_amd64
 set OUTDIR=%APPVEYOR_BUILD_FOLDER%\bin-%VC%-x64
-call build-x64.cmd 2>%APPVEYOR_BUILD_FOLDER%\error-%VC%.txt 1>%APPVEYOR_BUILD_FOLDER%\output-%VC%.txt
+echo Building x64
+call build-x64.cmd 1>%APPVEYOR_BUILD_FOLDER%\build-%VC%-x64.txt 2>&1
 goto %NEXT%
 
 :build_sdk
@@ -48,7 +51,8 @@ set PATH=%OPATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Release /ia64 /xp
 set OUTDIR=%APPVEYOR_BUILD_FOLDER%\bin-%VC%-ia64
-call build-ia64.cmd 2>%APPVEYOR_BUILD_FOLDER%\error-%VC%.txt 1>%APPVEYOR_BUILD_FOLDER%\output-%VC%.txt
+echo Building ia64
+call build-ia64.cmd 1>%APPVEYOR_BUILD_FOLDER%\build-%VC%-ia64.txt 2>&1
 goto %NEXT%
 
 :end
@@ -56,3 +60,5 @@ cd %APPVEYOR_BUILD_FOLDER%
 set > env.txt
 7z a %APPVEYOR_PROJECT_NAME%-%APPVEYOR_BUILD_VERSION%.7z bin-* *.txt
 
+rem IF EXIST error.txt exit 1
+exit 0
--- CPP/build-x32.cmd
+++ CPP/build-x32.cmd
@@ -1,87 +1,106 @@
-@echo on
+@echo off
 
 set ROOT=%cd%\7zip
 if not defined OUTDIR set OUTDIR=%ROOT%\bin32
 mkdir %OUTDIR%
 
-set OPTS=MY_STATIC_LINK=1
+set OPTS=MY_STATIC_LINK=1 /NOLOGO
 set LFLAGS=/SUBSYSTEM:WINDOWS,"5.01"
 
 cd %ROOT%\Bundles\Format7zExtract
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zxa.dll" >> %ERRFILE%
 copy O\7zxa.dll %OUTDIR%\7zxa.dll
 
 cd %ROOT%\Bundles\Format7z
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7za.dll" >> %ERRFILE%
 copy O\7za.dll %OUTDIR%\7za.dll
 
 cd %ROOT%\Bundles\Format7zF
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7z.dll" >> %ERRFILE%
 copy O\7z.dll %OUTDIR%\7z.dll
 
 cd %ROOT%\Bundles\Format7zFO
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zOrig.dll" >> %ERRFILE%
 copy O\7z.dll %OUTDIR%\7zOrig.dll
 
 cd %ROOT%\Bundles\Format7zUSB
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zUSB.dll" >> %ERRFILE%
 copy O\7zu.dll %OUTDIR%\7zu-x32.dll
 
 cd %ROOT%\UI\FileManager
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zFM.exe" >> %ERRFILE%
 copy O\7zFM.exe %OUTDIR%\7zFM.exe
 
 cd %ROOT%\UI\GUI
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zG.exe" >> %ERRFILE%
 copy O\7zG.exe %OUTDIR%\7zG.exe
 
 cd %ROOT%\UI\Explorer
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7-zip.dll" >> %ERRFILE%
 copy O\7-zip.dll %OUTDIR%\7-zip.dll
 
 cd %ROOT%\Bundles\SFXWin
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7z.sfx" >> %ERRFILE%
 copy O\7z.sfx %OUTDIR%\7z.sfx
 
 cd %ROOT%\Bundles\Codec_brotli
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ brotli-x32.dll" >> %ERRFILE%
 copy O\brotli.dll %OUTDIR%\brotli-x32.dll
 
 cd %ROOT%\Bundles\Codec_lizard
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ lizard-x32.dll" >> %ERRFILE%
 copy O\lizard.dll %OUTDIR%\lizard-x32.dll
 
 cd %ROOT%\Bundles\Codec_lz4
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ lz4-x32.dll" >> %ERRFILE%
 copy O\lz4.dll %OUTDIR%\lz4-x32.dll
 
 cd %ROOT%\Bundles\Codec_lz5
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ lz5-x32.dll" >> %ERRFILE%
 copy O\lz5.dll %OUTDIR%\lz5-x32.dll
 
 cd %ROOT%\Bundles\Codec_zstdF
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ zstd-x32.dll" >> %ERRFILE%
 copy O\zstd.dll %OUTDIR%\zstd-x32.dll
 
 cd %ROOT%\..\..\C\Util\7zipInstall
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ Install-x32.exe" >> %ERRFILE%
 copy O\7zipInstall.exe %OUTDIR%\Install-x32.exe
 
 cd %ROOT%\..\..\C\Util\7zipUninstall
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ Uninstall.exe" >> %ERRFILE%
 copy O\7zipUninstall.exe %OUTDIR%\Uninstall.exe
 
 set LFLAGS=/SUBSYSTEM:CONSOLE,"5.01"
 cd %ROOT%\UI\Console
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7z.exe" >> %ERRFILE%
 copy O\7z.exe %OUTDIR%\7z.exe
 
 cd %ROOT%\Bundles\SFXCon
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7zCon.sfx" >> %ERRFILE%
 copy O\7zCon.sfx %OUTDIR%\7zCon.sfx
 
 cd %ROOT%\Bundles\Alone
 nmake %OPTS%
+IF %errorlevel% NEQ 0 echo "Error x32 @ 7za.exe" >> %ERRFILE%
 copy O\7za.exe %OUTDIR%\7za.exe
 
 :ende
