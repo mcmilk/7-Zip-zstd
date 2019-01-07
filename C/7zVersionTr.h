ea4589d20d4cae7b103e10595dca8bbe92784263
Tino Reichardt <milky-7zip@mcmilk.de>
2019-01-07 20:55:17 +0100

    Fix 1/2 of issue #66

--- C/7zVersionTr.h
+++ C/7zVersionTr.h
@@ -5,7 +5,7 @@
 #define MY_AUTHOR_NAME  "Tino Reichardt"
 
 #undef  MY_COPYRIGHT
-#define MY_COPYRIGHT    "Copyright (c) 2016 - 2018 Tino Reichardt"
+#define MY_COPYRIGHT    "Copyright (c) 2016 - 2019 Tino Reichardt"
 
 #undef  MY_COPYRIGHT_DATE
 #define MY_COPYRIGHT_DATE MY_COPYRIGHT " : " MY_DATE
