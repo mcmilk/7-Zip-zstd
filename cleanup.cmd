6eb40bd4362d766d4d4dd48d0fdfd53000e9fd1e
conor42 <ronoc74@yahoo.com>
2019-03-29 00:12:55 +1000

    Rename O to x86 and add workaround to keep arm\7zCrcOpt.asm

--- cleanup.cmd
+++ cleanup.cmd
@@ -1,6 +1,8 @@
 @echo off
 
-FOR /R .\ %%d IN (arm x64 O) DO rd /S /Q %%d 2>NUL
+ren Asm\arm arm_
+FOR /R .\ %%d IN (arm x64 x86) DO rd /S /Q %%d 2>NUL
+ren Asm\arm_ arm
 
 del "CPP\7zip\*.7z" 2>NUL
 del "CPP\7zip\*.exe" 2>NUL
