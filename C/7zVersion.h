424714a7001fd49b37e63fef237aff22fcc30411
Tino Reichardt <milky-7zip@mcmilk.de>
2019-11-03 11:37:39 +0100

    Update lz4 to version 1.9.2 and some version numbers

--- C/7zVersion.h
+++ C/7zVersion.h
@@ -10,7 +10,7 @@
   #define MY_VERSION_CPU MY_VERSION
 #endif
 
-#define MY_DATE "2019-10-10"
+#define MY_DATE "2019-11-03"
 #undef MY_COPYRIGHT
 #undef MY_VERSION_COPYRIGHT_DATE
 #define MY_AUTHOR_NAME "Igor Pavlov, Tino Reichardt"
