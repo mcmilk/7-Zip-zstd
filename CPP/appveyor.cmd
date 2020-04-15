e68922f917a2f8812c6e7b3fd54e0192d3a3ef5a
Tino Reichardt <milky-7zip@mcmilk.de>
2020-04-15 11:48:18 +0200

    Use Visual Studio 2019

--- CPP/appveyor.cmd
+++ CPP/appveyor.cmd
@@ -6,34 +6,35 @@ REM Microsoft Visual Studio 2012 (VC=11.0)
 REM Microsoft Visual Studio 2013 (VC=12.0)
 REM Microsoft Visual Studio 2015 (VC=14.0)
 REM Microsoft Visual Studio 2017 (VC=15.0)
+REM Microsoft Visual Studio 2019 (VC=16.0)
 
 REM to many vcvarsall.cmd calls will blow it up!
 set OPATH=%PATH%
 set ERRFILE=%APPVEYOR_BUILD_FOLDER%\error.txt
 cd %APPVEYOR_BUILD_FOLDER%\CPP
 
-goto build_vs2017
+goto build_vs2019
 
-:build_vs2017
-set VC=15.0
+:build_vs2019
+set VC=16.0
 set PATH=%OPATH%
 set SUBSYS="5.01"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 call build-it.cmd
 
 set PATH=%OPATH%
 set SUBSYS="5.02"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 call build-it.cmd
 
 set PATH=%OPATH%
 set SUBSYS="6.02"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm
 call build-it.cmd
 
 set PATH=%OPATH%
 set SUBSYS="6.02"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64
 call build-it.cmd
 
 goto end
