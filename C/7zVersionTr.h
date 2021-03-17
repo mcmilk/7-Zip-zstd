f59c42a811b8a6770f7894e13832bc5c09b162d4
Tino Reichardt <milky-7zip@mcmilk.de>
2021-03-17 20:42:03 +0100

    Update zstd to version 1.4.9

--- C/7zVersionTr.h
+++ C/7zVersionTr.h
@@ -5,7 +5,7 @@
 #define MY_AUTHOR_NAME  "Tino Reichardt"
 
 #undef  MY_COPYRIGHT
-#define MY_COPYRIGHT    "Copyright (c) 2016 - 2020 Tino Reichardt"
+#define MY_COPYRIGHT    "Copyright (c) 2016 - 2021 Tino Reichardt"
 
 #undef  MY_COPYRIGHT_DATE
 #define MY_COPYRIGHT_DATE MY_COPYRIGHT " : " MY_DATE
