057ff04d5c8ff72da787bcf0af6ac3d8b1fb7283
sebres <info@sebres.de>
2025-02-26 13:02:53 +0100

    fixes zip decompression with zstd method, at the moment `SetDecoderProperties2` for zstd sets 1 byte flags only (sizes 3 and 5 retained for backwards compatibility, because have been never used previously); closes gh-391

--- CPP/7zip/Compress/ZstdDecoder.cpp
+++ CPP/7zip/Compress/ZstdDecoder.cpp
@@ -29,13 +29,21 @@ CDecoder::~CDecoder()
 
 Z7_COM7F_IMF(CDecoder::SetDecoderProperties2(const Byte * prop, UInt32 size))
 {
-  DProps *pProps = (DProps *)prop;
-
-  if (size >= 1 && size <= sizeof(DProps)) {
-    memcpy(&_props, pProps, size);
+  switch (size) {
+  case 1:
+    _props._flags = *prop;
+    // flags currently unused, todo: maybe some handling is necessary if set to not 0
+    // if (_props._flags != 0) ...
     return S_OK;
+  // for backwards compatibility only:
+  // size 3 or 5 was accepted in previous versions, so allow it here too
+  // (since the old props members _ver/_level seemed to be unused anyway)
+  case 3:
+  case 5:
+    return S_OK;
+  default:
+    return E_NOTIMPL;
   }
-  return E_NOTIMPL;
 }
 
 HRESULT CDecoder::SetOutStreamSizeResume(const UInt64 * /*outSize*/)
