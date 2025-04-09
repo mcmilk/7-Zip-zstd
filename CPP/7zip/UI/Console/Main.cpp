e939ae9603636f2855626848ba31959e319c0300
sebres <info@sebres.de>
2025-04-10 01:25:33 +0200

    7z extended with --version command; restrict max-compression test for x64 only (otherwise may exceed max process memory limit - 2GB for x86 in windows ABI)

--- CPP/7zip/UI/Console/Main.cpp
+++ CPP/7zip/UI/Console/Main.cpp
@@ -123,9 +123,10 @@ DECLARE_AND_SET_CLIENT_VERSION_VAR
 #endif
 
 
-static const char * const kCopyrightString = "\n7-Zip"
-  PROG_POSTFIX_2
-  " " MY_VERSION_CPU
+#define kVersionString "7-Zip" PROG_POSTFIX_2 " " MY_VERSION_CPU
+
+static const char * const kCopyrightString = "\n"
+  kVersionString
   " : " MY_COPYRIGHT_DATE "\n";
 
 static const char * const kHelpString =
@@ -145,6 +146,7 @@ static const char * const kHelpString =
     "  t : Test integrity of archive\n"
     "  u : Update files to archive\n"
     "  x : eXtract files with full paths\n"
+    "  --version : Show version information\n"
     "\n"
     "<Switches>\n"
     "  -- : Stop switches and @listfile parsing\n"
@@ -878,6 +880,11 @@ int Main2(
   if (commandStrings.Size() == 0)
   {
     ShowCopyrightAndHelp(g_StdStream, true);
+    return 0;
+  }
+
+  if (commandStrings.Size() == 1 && commandStrings[0] == L"--version") {
+    *g_StdStream << kVersionString;
     return 0;
   }
 
