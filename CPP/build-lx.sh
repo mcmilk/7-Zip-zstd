c360dd79b9b0fb3c86c6410fb0a30fd4936cb909
Tino Reichardt <milky-7zip@mcmilk.de>
2025-12-30 09:53:03 +0100

    Add variable to build-lx.sh, for easier switching between compilers

--- CPP/build-lx.sh
+++ CPP/build-lx.sh
@@ -9,13 +9,13 @@ function build() {
 
   case "$ARCH" in
     x86_64)
-      make CC=clang CXX=clang++ -j$((nproc-1)) -f makefile.gcc IS_X64=1 USE_ASM=1 MY_ASM=uasm || exit 1
+      make CC=$CC CXX=$CXX -j$((nproc-1)) -f makefile.gcc IS_X64=1 USE_ASM=1 MY_ASM=uasm || exit 1
       ;;
     i386)
-      make CC=clang CXX=clang++ -j$((nproc-1)) -f makefile.gcc IS_X86=1 USE_ASM=0
+      make CC=$CC CXX=$CXX -j$((nproc-1)) -f makefile.gcc IS_X86=1 USE_ASM=0
       ;;
     aarch64)
-      make CC=clang CXX=clang++ -j$((nproc-1)) -f makefile.gcc IS_ARM64=1 USE_ASM=0
+      make CC=$CC CXX=$CXX -j$((nproc-1)) -f makefile.gcc IS_ARM64=1 USE_ASM=0
       ;;
     *)
       echo "Unknown arch: $ARCH"
@@ -31,6 +31,11 @@ function build() {
 
 export OUTDIR="$PWD/build"
 export ARCH=$(arch)
+export CC="gcc"
+export CXX="g++"
+
+export CC="clang"
+export CXX="clang++"
 mkdir -p "$OUTDIR"
 
 # standalone, minimalistic (flzma2, zstd)
